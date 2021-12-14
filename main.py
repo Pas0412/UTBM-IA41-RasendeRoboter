b581ad955212508e206b33b0ff2bd99072ac6e9c
yonghui <yonghuihuang412@gmail.com>
2021-12-14 15:02:34 +0100

    install pygame

--- main.py
+++ main.py
@@ -1,4 +1,1 @@
-import numpy
-class Rasende:
-    direction = {"N": (-1, 0), "E": (0, 1), "S": (1,0), "W": (0, -1)}
-    postion_by_color = {"red" : 0, "yellow": 1, "green": 2, "blue": 3}
+import pygame
